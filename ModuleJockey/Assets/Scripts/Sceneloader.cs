bf64cc58c70a648e2eee8740e7ab5bf71eb463cf
VajcoTV <jakub.gulvas@gmail.com>
2021-02-13 17:13:01 +0100

    upratal bordel a urboil este vacsi

---
+++ ModuleJockey/Assets/Scripts/Sceneloader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneloader : MonoBehaviour
+{
+    Scene currentscene;
+    Scene previousscene;
+    
+    private void Awake()
+    {
+        currentscene = SceneManager.GetActiveScene();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentscene.buildIndex + 1);
+    }
+    public void LoadMainMenu()
+    {
+        previousscene = SceneManager.GetActiveScene();
+        
+        SceneManager.LoadScene(0);
+    }
+    public void LoadMainMenufromTown()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadTown()
+    {
+        previousscene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(1);
+    }
+   
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void ReloadScene()
+    {
+        currentscene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentscene.name);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player destroyer"))
+        {
+            ReloadScene();
+        }
+    }
+    
+}

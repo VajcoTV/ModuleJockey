5410f3b4f6e5e96a5f7bf8fe049610a9f2f73885
VajcoTV <jakub.gulvas@gmail.com>
2021-05-11 17:23:10 +0200

    PointLight Mushoooms

---
+++ ModuleJockey/Assets/Scripts/Managers/LightManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    [SerializeField] Color Ncolor;
+    [SerializeField] Color Ccolor;
+    public UnityEngine.Experimental.Rendering.Universal.Light2D SUN;
+
+    private void Start()
+    {
+        
+        SwitchManager.NormalSwitch += PlayerNormalDimensionSwitch;
+        SwitchManager.MagicSwitch += PlayerMagicDimensionSwitch;
+        SUN = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+    }
+
+    void PlayerNormalDimensionSwitch()
+    {
+        SUN.color = Ncolor;
+    }
+    void PlayerMagicDimensionSwitch()
+    {
+        SUN.color = Ccolor;
+    }
+}

c813e073dba4a165c9b7f6e264c186291f3401bf
VajcoTV <jakub.gulvas@gmail.com>
2021-05-12 20:03:14 +0200

    new enemy behaviour

--- ModuleJockey/Assets/Scripts/Enemy/ZelenyDuch/Zelney ducho/ZelenyDuchPatrol.cs
+++ ModuleJockey/Assets/Scripts/Enemy/ZelenyDuch/Zelney ducho/ZelenyDuchPatrol.cs
@@ -7,16 +7,18 @@ public class ZelenyDuchPatrol : StateMachineBehaviour
     Rigidbody rb;
     float speed = 10;
     Transform enemy;
-    float radius = 5;
+    
     Vector3 PatrolPos;
     bool canchase = true;
+    bool newposition = true;
+    float timer = 3;
+    Vector3 newpos;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = app.playermanager.player;
         rb = animator.GetComponent<Rigidbody>();
         enemy = animator.GetComponent<Transform>();
-        PatrolPos = player.GetComponent<Movement>().PlayerRadius(enemy.position.y);
         canchase = true;
 
     }
@@ -24,13 +26,29 @@ public class ZelenyDuchPatrol : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Vector3 newpos = Vector3.MoveTowards(rb.position, PatrolPos, speed * Time.deltaTime);
+        if (newposition)
+        {
+            PatrolPos = player.GetComponent<Movement>().PlayerRadius(enemy.position.y);
+            Debug.Log("Yes Nova pozicia");
+            newposition = false;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                timer = 3;
+                newposition = true;
+            }
+        }
+        newpos = Vector3.MoveTowards(rb.position, PatrolPos, speed * Time.deltaTime);
         rb.MovePosition(newpos);
+
         if (Vector3.Distance(player.transform.position, enemy.transform.position) < 1f)
         {
             animator.SetTrigger("Attack");
         }
-        if (Vector3.Distance(player.transform.position, enemy.transform.position) > 20f)
+        if (Vector3.Distance(player.transform.position, enemy.transform.position) > 40f)
         {
             animator.SetBool("Run", false);
         }
@@ -39,7 +57,7 @@ public class ZelenyDuchPatrol : StateMachineBehaviour
             player.GetComponent<Movement>().numberofenemy += 1;
             animator.SetBool("Chase", true);
             canchase = false;
-            Debug.Log("Lol pod sem zmrde");
+           
         }
 
         if (player.transform.position.x < enemy.position.x)
@@ -58,8 +76,9 @@ public class ZelenyDuchPatrol : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetTrigger("Stop");
+        newposition = true;
     }
+    
 
 
 
